8a272826c2c4276f9a5df010e5618508549b11b5
Aditya Singh <asaditya2002@gmail.com>
2023-02-06 22:57:33 +0530

    Fixed Landing Page Style Error

--- src/Components/HomePage/MainPage.jsx
+++ src/Components/HomePage/MainPage.jsx
@@ -6,7 +6,7 @@ function MainPage() {
     return (
         <>
             <div className='w-screen flex md:flex-row flex-col-reverse'>
-                <div className='md:w-[50%] w-[217vw] bg-[#f04b4f] h-[762px] pl-16 flex justify-center flex-col text-white'>
+                <div className='md:w-[50%] w-[217vw] bg-[#f04b4f] h-[92vh] pl-16 flex justify-center flex-col text-white'>
                     <h1 className='font-Rubik md:text-[39px] text-5xl md:pb-1 pb-3 font-bold'>FLAT 40% OFF</h1>
                     <h2 className='md:text-xl text-6xl md:pb-3 pb-6'>END OF SEASON SALE</h2>
                     <span className='flex flex-col items-start font-bold md:text-[15px] text-3xl underline md:space-y-3 space-y-7'>
@@ -16,7 +16,7 @@ function MainPage() {
                     </span>
                 </div>
                 <div className='md:w-[50%] w-[217vw]'>
-                    <Image src={'/MainPageImg.png'} placeholder='empty' width={2000} height={490} />
+                    <Image src={'/MainPageImg.png'} placeholder='empty' width={2000} height={500} />
                 </div>
             </div>
         </>
